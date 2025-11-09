10a3ced4fe2ea23451bfb9dab840c4dd8f25f9ff
CodeByDante <gonzalosalvadorloyolahuarauya@gmail.com>
2025-11-09 15:32:30 -0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -35,7 +35,7 @@ export async function uploadToImgBB(file: File): Promise<string | null> {
 
     const formData = new FormData();
     formData.append('image', file);
-    formData.append('key', '3b5b5605ac5c1676c75cfc907c47eb58');
+    formData.append('key', 'b05c975abc1f6d575a93d1c0a73839bc');
 
     const response = await fetch(API_CONFIG.IMGBB.BASE_URL, {
       method: 'POST',
